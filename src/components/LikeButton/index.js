dbb7da191cdadd10b37bc6541f5a5648f033272a
Gabriel Moslaves <gabrielmoslaves@gmail.com>
2022-04-12 17:17:57 -0300

    responsivo

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -1,17 +1,19 @@
 import { useState } from "react";
-import heart from '../../images/heart.png'
-import heartfilled from '../../images/heartfilled.png'
-
+import heart from "../../images/heart.png";
+import heartfilled from "../../images/heartfilled.png";
 
 function LikeButton() {
   const [like, setLike] = useState(false);
 
   return (
-  <div>
-    {like ? (<img onClick= {()=>setLike(!like)} src={heartfilled}/>
-) : (
-<img onClick= {()=>setLike(!like)} src={heart}/>)}
-  </div>);
+    <div>
+      {like ? (
+        <img onClick={() => setLike(!like)} src={heartfilled} />
+      ) : (
+        <img onClick={() => setLike(!like)} src={heart} />
+      )}
+    </div>
+  );
 }
 
 export default LikeButton;
